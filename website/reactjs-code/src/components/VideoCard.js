7ecd0686c19109ae9d5565694570f87a8b0b1ab7
Shivendra <shivharsh44@gmail.com>
2023-11-14 11:14:38 +0530

    new modifications

--- website/reactjs-code/src/components/VideoCard.js
+++ website/reactjs-code/src/components/VideoCard.js
@@ -4,48 +4,62 @@ import BottomNav from './BottomNav';
 
 export default function VideoCard(props) {
   const [audioUrl, setAudioUrl] = useState(null);
-  const [bottomNav, setbottomNav] = useState(null);
+  const [bottomNav, setBottomNav] = useState(null);
 
   const handlePlay = async () => {
-    const videoId = extractVideoId(props.videoUrl);
+    const { videoUrl, duration } = props;
 
-    if (videoId) {
-      const url = `https://www.youtube.com/watch?v=${videoId}`;
-      console.log(url)
-      setAudioUrl(url);
-      setbottomNav(' ');
+    if (videoUrl) {
+      try {
+        const audioUrl = `https://www.youtube.com/watch?v=${videoUrl}&t=${duration || '0s'}`;
+        console.log('audio Url fetching worked ' + audioUrl)
+        setAudioUrl(audioUrl);
+        setBottomNav(true);
 
-      return url;
+        return audioUrl;
+      } catch (error) {
+        console.error(error);
+        setAudioUrl(null);
+        setBottomNav(false);
+        return null;
+      }
     } else {
       setAudioUrl(null);
+      setBottomNav(false);
+
       return null;
     }
   };
 
-  function extractVideoId(url) {
-    const regex = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
-  }
-
   return (
     <>
       <div className="container m-auto">
-        <div className="card text-start mb-3 mx-auto" onClick={handlePlay} style={{ maxWidth: '80%', cursor: 'pointer' }}>
+        <div
+          className="card text-start mb-3 mx-auto"
+          onClick={handlePlay}
+          style={{ maxWidth: '80%', cursor: 'pointer' }}
+        >
           <div className="row g-0">
             <div className="col-md-4 col-xs-4">
-              <img src={props.imageUrl} className="object-fit-cover img-fluid img-thumbnail" style={{ objectFit: 'contain' }} alt="..." />
+              <img
+                src={props.imageUrl}
+                className="object-fit-cover img-fluid img-thumbnail"
+                style={{ objectFit: 'contain' }}
+                alt="..."
+              />
             </div>
             <div className="col-md-8 col-xs-8">
               <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.channel}</p>
-                <p className="card-text"><small className="text-body-secondary">hello</small></p>
+                <p className="card-text">
+                  <small className="text-body-secondary">hello</small>
+                </p>
               </div>
             </div>
           </div>
         </div>
-        {bottomNav && <BottomNav state={bottomNav} audioUrl={audioUrl} audioTitle={props.title}/>}
+        {bottomNav && <BottomNav audioUrl={audioUrl} audioTitle={props.title} />}
       </div>
     </>
   );
